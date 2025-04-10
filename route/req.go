0378c7370f37f2a8e4bcd629cfff8b53b4530a1f
charlie <charlie@choco-up.com>
2025-04-10 13:21:48 +0800

    make body read callable instead of automatically

--- route/req.go
+++ route/req.go
@@ -1,8 +1,13 @@
 package route
 
 import (
+	"bufio"
+	"errors"
+	"io"
 	"net"
-
+	"strconv"
+	"sync"
+	"unicode/utf8"
 )
 
 type Req struct {
@@ -10,26 +15,82 @@ type Req struct {
 	Url             string
 	Protocol        string
 	ProtocolVersion string
-	Ahs             map[string]string
-	Conn            net.Conn
-    Body            []byte
+	ahs             map[string]string
+	conn            net.Conn
+	body            []byte
+	textBody        string
 }
 
 func NewReq(method, url, protocol, protocolVersion string, ahs map[string]string, conn net.Conn) *Req {
-    return &Req{
+	return &Req{
 		Method:          method,
 		Url:             url,
 		Protocol:        protocol,
 		ProtocolVersion: protocolVersion,
-		Ahs:             ahs,
-		Conn:            conn,
-        Body:            nil,
+		ahs:             ahs,
+		conn:            conn,
+		body:            nil,
 	}
 }
 
-// read as text
+func (r *Req) ReadBody() ([]byte, error) {
+	ov := sync.OnceValue(func() error {
+		cl := r.GetHeader("Content-Length")
+		if cl == "" {
+			return errors.New("Content length not specified in request")
+		}
 
-// read json
+		clInt, err := strconv.Atoi(cl)
+		if err != nil {
+			return errors.New("Content length malformed")
+		}
 
-// read byte array
+		bs := make([]byte, clInt)
+		bufIoReader := bufio.NewReader(r.conn)
+		_, err = io.ReadFull(bufIoReader, bs)
+		if err != nil {
+			return errors.New("Body malformed")
+		}
 
+		valid := utf8.Valid(bs)
+		if !valid {
+			return errors.New("Body malformed")
+		}
+		if len(bs) == 0 {
+			return errors.New("Body malformed")
+		}
+
+		r.body = bs
+		return nil
+	})
+	err := ov()
+	if err != nil {
+		return nil, err
+	}
+
+	return r.body, nil
+}
+
+func (r *Req) ReadTextBody() (string, error) {
+	ov := sync.OnceValue(func() error {
+		bs, err := r.ReadBody()
+		if err != nil {
+			return err
+		}
+		r.textBody = string(bs)
+		return nil
+	})
+	err := ov()
+	if err != nil {
+		return "", err
+	}
+	return r.textBody, nil
+}
+
+func (r *Req) GetHeader(key string) string {
+	h, ok := r.ahs[key]
+	if ok {
+		return h
+	}
+	return ""
+}
